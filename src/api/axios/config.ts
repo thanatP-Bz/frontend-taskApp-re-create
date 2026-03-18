c91fe651182ea9b302a296252d8b872285a4fcc4
Thanat <bztzn512@gmail.com>
2026-03-18 12:21:56 -0400

    add backend link

--- src/api/axios/config.ts
+++ src/api/axios/config.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://backend-taskapp-re-create.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
